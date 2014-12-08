1938289bc431dffeb198df6c94d589575ed7c5f2
mntj <mntj11@gmail.com>
2014-12-08 16:31:08 -0500

    Add Dashboard route, controller, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shouter::Application.routes.draw do
   root to: 'homes#show', via: :get
+  resource :dashboard, only: [:show]
   # Added by monban
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
